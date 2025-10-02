6d0c9e0d23032d4f9cb059f70144cd4c35d57143
Santiago L. Romero <santiago_pna@hotmail.com>
2025-10-02 18:30:35 -0300

    url patterns media

--- ProyectoDjango/ProyectoDjango/urls.py
+++ ProyectoDjango/ProyectoDjango/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +26,4 @@ urlpatterns = [
 ]
 
 # Ruta imágenes
-if settings.DEBUG:    
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
